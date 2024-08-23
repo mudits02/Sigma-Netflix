5bfb2758c9a1fe69d642846241724cfc4a8f5fc6
mudits02 <mudits02@gmail.com>
2024-08-24 00:52:06 +0530

    BAsic Landing Page UI

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,13 @@
-const Header = () =>{
-    return(
-        <div className="header">
+import { LOGO_URL } from "../Utils/Constants";
 
+const Header = () => {
+    return (
+        <div className="absolute top-0 left-0 w-full px-8 py-4 bg-gradient-to-b from-black to-transparent z-10">
+            <img 
+                className="w-36"
+                src={LOGO_URL}
+                alt="logo"
+            />
         </div>
     )
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,55 @@
-import { LOGO_URL } from "../Utils/Constants";
-import Header from "./Header"
+import { BG_URL } from "../Utils/Constants";
+import Header from "./Header";
+import { useState } from "react";
 
 const Login = () => {
-    return(
-        <div className="login">
+
+    const [isSignInForm, setSignInForm] = useState(true);
+
+    const toggleSignInForm = () => {
+        setSignInForm(!isSignInForm);
+    }
+
+    return (
+        <div className="relative h-screen w-screen flex items-center justify-center bg-black">
             <Header />
-            <img
-                src = {LOGO_URL}
-                alt = "Logo"
-            />
+            <div className="absolute top-0 left-0 w-full h-full">
+                <img
+                    src={BG_URL}
+                    alt="Background"
+                    className="w-full h-full object-cover opacity-80"
+                />
+            </div>
+
+            <form className="relative z-20 bg-black bg-opacity-75 p-12 rounded-md max-w-md w-full text-white">
+                <h1 className="text-4xl mb-8">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+                <input 
+                    type="text" 
+                    placeholder="Email" 
+                    className="w-full p-4 my-4 bg-gray-800 rounded text-lg" 
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    className="w-full p-4 my-4 bg-gray-800 rounded text-lg" 
+                />
+                {!isSignInForm && (
+                    
+                        <input 
+                            type="text" 
+                            placeholder="Name" 
+                            className="w-full p-4 my-4 bg-gray-800 rounded text-lg" 
+                        />
+                    
+                )}
+                <button 
+                    className="w-full p-4 mt-4 bg-red-600 rounded text-lg hover:bg-red-700 transition-colors duration-300">
+                    {isSignInForm ? "Sign In" : "Sign Up"}
+                </button>
+                <p className="py-10 font-bold cursor-pointer" onClick={toggleSignInForm}>
+                    {isSignInForm ? "New to Netflix? Sign Up Now" : "Already a Member? Sign In Now"}
+                </p>
+            </form>
         </div>
     )
 }
