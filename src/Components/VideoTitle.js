f63ca3adf4d933948df766044bdbc5efbc7b971a
mudits02 <mudits02@gmail.com>
2024-09-09 22:04:12 +0530

    Full screen video now autoplays

--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -1,14 +1,17 @@
-const VideoTitle = ({title , overview}) => {
-    return(
-        <div className="pt-36 px-12">
-            <h1 className="text-6xl font-bold">{title}</h1>
-            <p className="text-lg py-6 w-1/4">{overview}</p>
-            <div>
-                <button className="bg-gray-400 text-xl text-white p-4 px-16 rounded-lg ">▶️ Play</button>
-                <button className="mx-2 bg-gray-400 text-xl text-white p-4 px-16">More Info</button>
-            </div>
+const VideoTitle = ({ title, overview }) => {
+    return (
+      <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black">
+        <h1 className="text-6xl font-bold">{title}</h1>
+        <p className="py-6 text-lg w-1/4">{overview}</p>
+        <div className="">
+          <button className=" bg-white text-black p-4 px-12 text-xl  rounded-lg hover:bg-opacity-80">
+            ▶️ Play
+          </button>
+          <button className="mx-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">
+            More Info
+          </button>
         </div>
-    )
-}
-
-export default VideoTitle;
+      </div>
+    );
+  };
+  export default VideoTitle;
--- src/Hooks/useMovieTrailer.js
+++ src/Hooks/useMovieTrailer.js
@@ -8,13 +8,12 @@ const useMovieTrailer = (movieId) => {
     const dispatch = useDispatch();
 
     const getMovieTrailer =  async() => {
-        const data = await fetch("https://api.themoviedb.org/3/movie/"+movieId+ "/videos?language=en-US", API_OPTIONS)
+        const data = await fetch("https://api.themoviedb.org/3/movie/"+ movieId + "/videos?language=en-US", API_OPTIONS)
         const json = await data.json();
 
         const filterData = json.results.filter(video => video.type == "Trailer");
         const trailerArray = filterData.length ? filterData.filter(video => video.name == 'Official Trailer') : json.results[0].filter(video => video.name == 'Official Trailer');
-        const trailer = trailerArray[0];
-        
+        const trailer = trailerArray[0];        
         dispatch(addVideoTrailer(trailer))
     }
 
